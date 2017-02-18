b34fa1763d53179d91f63a7ee2b6c41477b71a11
Sebastian Sandqvist <seb@sparque.me>
2017-02-17 20:13:13 -0800

    dictionary update, minor changes

--- src/views/_App.js
+++ src/views/_App.js
@@ -4,6 +4,8 @@ import dictionary from '../dictionary.js';
 import Word from './Word.js';
 
 function getWordClass(word) {
+	// console.log(word);
+	// if (word === '\n') { return 'newLine'; }
 	if (dictionary[word] === undefined) { return ''; }
 	if (dictionary[word].length === 1 && dictionary[word][0] === '-') {
 		return 'noReplacements';
@@ -15,15 +17,20 @@ function getReplacements(word) {
 	return dictionary[word] || [];
 }
 
+function isolateWord(word) {
+	return word.toLowerCase().replace(/[^0-9a-z]/g, '');
+}
+
 const model = {
 	inputText: stream(''),
 	parsedText: [],
 	parse() {
+		// console.log(model.inputText().match(/\w+/g));
 		model.parsedText = model.inputText().split(' ').map(function(word) {
 			return {
 				word,
-				wordClass: getWordClass(word.toLowerCase()),
-				replacements: getReplacements(word.toLowerCase())
+				wordClass: getWordClass(isolateWord(word)),
+				replacements: getReplacements(isolateWord(word))
 			};
 		});
 	}
@@ -39,7 +46,7 @@ const App = {
 					oninput: m.withAttr('value', model.inputText)
 				}),
 				m('.center.pad20',
-					m('button.Button', { onclick: model.parse }, 'Parse for Anglish origin')
+					m('button.Button', { onclick: model.parse }, 'Parse Text')
 				),
 				m('hr'),
 				m('.Output', model.parsedText.map((data) => m(Word, data)))
